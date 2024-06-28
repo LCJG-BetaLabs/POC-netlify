dcb6d658e4a5fbe36eb9976998bcd6e4bae9e199
nicholasyuen-lcjg <NicholasYuen@lcjgroup.com>
2024-06-29 00:01:49 +0800

    refactor: disable optimize images

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -26,14 +26,12 @@ export default function Products() {
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-700 to-violet-900">
           <p>Elsie Admin Portal</p>
-          {/* <Image
+          <img
             src="/adminPortal.png"
+            width="100%"
             alt="Picture of the adminPortal"
             className="object-cover object-left-top h-[60%]  md:h-[90%] absolute -bottom-10 inset-x-0 w-[90%] rounded-xl mx-auto"
-            width={1000}
-            height={1000}
-          /> */}
-          <img src="/adminPortal.png" width="100%" alt="Login V2" />
+          />
         </div>
       ),
     },
